8146a596a456ddefbcca65dd394d27e115160f95
melanifern-05 <melanif051@gmail.com>
2026-03-24 09:42:54 -0400

    .

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import sqlite3
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -37,4 +38,6 @@ def index():
     )
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=False)
